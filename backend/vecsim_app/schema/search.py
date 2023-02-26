83af6ac8758aeec05f63d361a723c67e50368338
Artem Yushkovskiy <artem.yushkovskiy@deliveryhero.com>
2023-02-26 19:40:33 +0100

    Revert backend/vecsim_app/schema/search.py

--- backend/vecsim_app/schema/search.py
+++ backend/vecsim_app/schema/search.py
@@ -5,7 +5,7 @@ class SimilarityRequest(BaseModel):
     paper_id: str
     categories: list
     years: list
-    number_of_results: int = 10
+    number_of_results: int = 15
     search_type: str = "KNN"
     categories_operator: str = "AND"
 
@@ -14,5 +14,5 @@ class UserTextSimilarityRequest(BaseModel):
     articles: list
     categories: list
     years: list
-    number_of_results: int = 10
+    number_of_results: int = 15
     search_type: str = "KNN"
